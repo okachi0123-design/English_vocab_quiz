1bc4bde766a885ba9f440bb1cc5359b108e66194
okachi0123-design <260033931+okachi0123-design@users.noreply.github.com>
2026-05-29 00:55:20 +0900

    Use relative path in put.data.test.sh and add simplequiz.sh script that shuffles the CSV and outputs random lines

--- put.data.test.sh
+++ put.data.test.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
 
-python wordqz.py > /home/okachi/English_vocab_quiz/csv.d/word.csv;python phraseqz.py > /home/okachi/English_vocab_quiz/csv.d/phrase.csv
+python wordqz.py > csv.d/word.csv
+
+python phraseqz.py > csv.d/phrase.csv
 
